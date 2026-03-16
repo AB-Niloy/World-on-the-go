7da94bd512cf84c25be47e52b1952a93867bac10
AB-Niloy <arnob080100@gmail.com>
2026-03-16 14:41:56 +0600

    Added style in react

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
-    console.log(country.population.population);
+    console.log(country.area.area);
+
+    const handleVisited = () => {
+        console.log('Button Clicked');
+    }
+
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Capital: {country.capital.capital}</p>
             <p>Region: {country.region.region}</p>
             <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? 'Big Country' : 'Small Country'}</p>
+            <button onClick={handleVisited}>Not Visited</button>
         </div>
     );
 };
