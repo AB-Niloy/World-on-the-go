cc9aa4d292d476990850feb8a65efe7612f60be4
AB-Niloy <arnob080100@gmail.com>
2026-03-16 14:05:44 +0600

    Display country flags and info of the country

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Country = () => {
+const Country = ({country}) => {
+    console.log(country.population.population);
     return (
         <div>
-            <h3>Name: </h3>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common}</h3>
+            <p>Capital: {country.capital.capital}</p>
+            <p>Region: {country.region.region}</p>
+            <p>Population: {country.population.population}</p>
         </div>
     );
 };
